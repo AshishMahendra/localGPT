792a135f3c3b4ca37ca7fbeb3ba2df1a82ba5b7d
Your Name <am028275@gmail.com>
2024-07-05 09:22:27 +0000

    added constantfile

---
+++ ask_monk/utils/constants.py
@@ -0,0 +1,21 @@
+import os
+from chromadb.config import Settings
+
+ROOT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+
+# Define the folder for storing database
+SOURCE_DIRECTORY = f"{ROOT_DIRECTORY}/SOURCE_DOCUMENTS"
+
+PERSIST_DIRECTORY = f"{ROOT_DIRECTORY}/DB"
+
+
+# Can be changed to a specific number
+INGEST_THREADS = os.cpu_count() or 8
+
+# Define the Chroma settings
+CHROMA_SETTINGS = Settings(
+    anonymized_telemetry=False,
+    is_persistent=True,
+)
+CONTEXT_WINDOW_SIZE = 4096
+MAX_NEW_TOKENS = CONTEXT_WINDOW_SIZE
